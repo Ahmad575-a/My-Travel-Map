a610367de9880946505c09e4a10723d25f9695ee
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-25 14:38:19 +0200

    configure dotenv and mongoose and add a pin route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const pinRoute =require('./routes/pins')
+// dotenv config
+require('dotenv').config()
+
+// mongoose config
+const DB_NAME = process.env.DB_NAME
+
+const DB_LINK = process.env.MONGO_LINK + DB_NAME
+mongoose.connect(DB_LINK, {
+    // hide the warnings in  the console because the default is false
+    useUnifiedTopology : true,
+    useNewUrlParser: true
+})
+.then(()=> console.log('MongoDB database is Successfully connected'))
+.catch(()=> console.log('Database connection failed!'))
 
 
+app.use('/pins',pinRoute)
 
 
 app.listen(5000,()=> console.log('server is running ...'))
