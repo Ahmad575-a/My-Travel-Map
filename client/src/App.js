6caa2818a2b831fdeb1dce43505571cc46cfafbd
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-27 23:57:26 +0200

    added axios to get the data from the backend, intilize the pin state and map the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { Room, Star } from "@material-ui/icons";
 import "./app.css"
+import axios from "axios";
+import { Fragment } from 'react';
 
 
 const App = () => {
+  const [pins, setPins] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -12,6 +15,19 @@ const App = () => {
     longitude: 17,
     zoom: 4
   });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
   return (
     <div>
       <ReactMapGL
@@ -20,6 +36,8 @@ const App = () => {
         onViewportChange={nextViewport => setViewport(nextViewport)}
         mapStyle="mapbox://styles/ahmad757-a/ckp7dbkxl0e6d17oa2rbtiz4x"
       >
+        {pins.map(p => (
+          <>
         <Marker
           latitude={51.756310}
           longitude={14.332868}
@@ -41,9 +59,9 @@ const App = () => {
           anchor="left" >
           <div className="card">
             <label>Place</label>
-            <h4 className="place">Cottbus</h4>
+            <h4 className="place">{p.title}</h4>
             <label>Review</label>
-            <p className="desc">Awesome place</p>
+            <p className="desc">{p.desc}</p>
             <label>Rating</label>
             <div className="stars">
               <Star className="star" />
@@ -54,11 +72,12 @@ const App = () => {
             </div>
             <label>Information </label>
             <span className="username">
-              Created by <b>Ahmad</b>
+                Created by <b>{p.username}</b>
             </span>
             <span className="date"> 1 hour ago</span>
           </div>
-        </Popup>
+        </Popup></>
+        ))}
       </ReactMapGL>
     </div>
   )
