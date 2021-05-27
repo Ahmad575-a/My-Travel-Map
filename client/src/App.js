183415a3499b46db963574f703efbeb41299eda2
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-27 23:24:54 +0200

    added a style to the map and popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import ReactMapGL, {Marker, Popup}  from 'react-map-gl';
-import { Room , Star} from "@material-ui/icons";
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room, Star } from "@material-ui/icons";
 import "./app.css"
 
 
@@ -15,49 +15,49 @@ const App = () => {
   return (
     <div>
       <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapStyle="mapbox://styles/ahmad757-a/ckp7dbkxl0e6d17oa2rbtiz4x"
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/ahmad757-a/ckp7dbkxl0e6d17oa2rbtiz4x"
       >
-        <Marker 
-        latitude={51.756310}
-        longitude={	14.332868}
-        offsetLeft={-20}
-        offsetTop={-10}>
+        <Marker
+          latitude={51.756310}
+          longitude={14.332868}
+          offsetLeft={-20}
+          offsetTop={-10}>
           <Room
-                style={{
-                  fontSize: 6 * viewport.zoom,
-                  color: "lime"
-                }}
-                
-              />
-      </Marker>
-      <Popup
+            style={{
+              fontSize: 6 * viewport.zoom,
+              color: "lime"
+            }}
+
+          />
+        </Marker>
+        <Popup
           latitude={51.756310}
           longitude={14.332868}
           closeButton={true}
           closeOnClick={false}
           anchor="left" >
           <div className="card">
-                  <label>Place</label>
-                  <h4 className="place">Cottbus</h4>
-                  <label>Review</label>
-                  <p className="desc">Awesome place</p>
-                  <label>Rating</label>
-                  <div className="stars">
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                  </div>
-                  <label>Information </label>
-                  <span className="username">
-                    Created by <b>Ahmad</b>
-                  </span>
-                  <span className="date"> 1 hour ago</span>
-                </div>
+            <label>Place</label>
+            <h4 className="place">Cottbus</h4>
+            <label>Review</label>
+            <p className="desc">Awesome place</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+            </div>
+            <label>Information </label>
+            <span className="username">
+              Created by <b>Ahmad</b>
+            </span>
+            <span className="date"> 1 hour ago</span>
+          </div>
         </Popup>
       </ReactMapGL>
     </div>
