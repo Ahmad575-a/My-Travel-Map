b824f3be5859ed280e51eaf633bf671a3eb7cdb4
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-28 23:40:41 +0200

    added a form for the new pin popup and try to post and get data from the server with axios with an err

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ const App = () => {
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [star, setStar] = useState(0);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -32,10 +35,30 @@ const App = () => {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating: star,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("http://localhost:5000/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     const getPins = async () => {
       try {
-        const res = await axios.get("/pins");
+        const res = await axios.get("http://localhost:5000/pins");
         setPins(res.data);
       } catch (err) {
         console.log(err);
@@ -64,7 +87,7 @@ const App = () => {
               <Room
                 style={{
                   fontSize: 6 * viewport.zoom,
-                  color: currentUsername === p.username ? "lime" : "red",
+                  color: currentUser === p.username ? "lime" : "red",
                   cursor: "pointer"
                 }}
                 onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
@@ -85,11 +108,7 @@ const App = () => {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
+                  {Array(p.rating).fill(<Star className="star" />)}
                   </div>
                   <label>Information </label>
                   <span className="username">
@@ -108,7 +127,34 @@ const App = () => {
             closeButton={true}
             closeOnClick={false}
             anchor="left"
-            onClose={() => setNewPlace(null)} >new</Popup>)}
+            onClose={() => setNewPlace(null)} >
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <label>Title</label>
+                  <input
+                    placeholder="Enter a title"
+                    autoFocus
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Say us something about this place."
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                  <label>Rating</label>
+                  <select onChange={(e) => setStar(e.target.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <button type="submit" className="submitButton">
+                    Add Pin
+                  </button>
+                </form>
+              </div>
+            </Popup>)}
       </ReactMapGL>
     </div>
   )
